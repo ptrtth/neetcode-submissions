fd531b5b0a33f8a7c545c9c795b3df08b29aebaf
ptrtth <120004813+ptrtth@users.noreply.github.com>
2026-07-17 20:45:47 +0200

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.go
@@ -0,0 +1,38 @@
+func execOp(op string, a int, b int) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		return 0
+	}
+}
+
+func evalRPN(tokens []string) int {
+	stack := []int{}
+
+	for _, token := range tokens {
+		number, err := strconv.Atoi(token)
+
+		if err == nil {
+			stack = append(stack, number)
+			continue
+		}
+
+		b := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		a := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		result := execOp(token, a, b)
+		stack = append(stack, result)
+	}
+
+	return stack[0]
+}
